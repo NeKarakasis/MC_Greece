a24d6c448ac333412ba008faa345ec7dcc1bbbd6
apostolisrenesas <apostolos.lamprokostopoulos.jy@renesas.com>
2025-02-26 17:38:20 +0200

    commit the initial sensorless FOC firmware provided from website without Tuner

---
+++ RA6T3_MCILV1_SPM_LESS_FOC_E2S_V101/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := C:/Temp/1st_release_check/RA6T3_MCILV1_SPM_LESS_FOC_E2S_V101/src/application/user_interface/ics/ICS2_RA6T3.o
+
+LIBS := -lrm_motor_current -lrm_motor_estimate -lrm_motor_speed
+
